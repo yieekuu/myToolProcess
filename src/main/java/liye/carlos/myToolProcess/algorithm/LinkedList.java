9906c14695dfecfb643f484c6ba468f5200ab1bf
liye.carlos <carlos@liyecarlosdeMacBook-Pro.local>
2017-06-19 13:32:21 +0800

    has Cycle

--- src/main/java/liye/carlos/myToolProcess/algorithm/LinkedList.java
+++ src/main/java/liye/carlos/myToolProcess/algorithm/LinkedList.java
@@ -4,4 +4,33 @@ package liye.carlos.myToolProcess.algorithm;
  * Created by liye3 on 2017/6/19.
  */
 public class LinkedList {
+
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+        ListNode front = head;
+        ListNode behind = head;
+        while (head.next != null && head.next.next != null) {
+            front = head.next.next;
+            behind = head.next;
+            head = head.next;
+            if (front == behind) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
