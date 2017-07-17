cd6092ea3589d1eb8e03b5c424a36f6f5430857b
yieekuu <liye.carlos@gmail.com>
2017-07-17 14:05:45 +0800

    add

--- src/main/java/liye/carlos/myToolProcess/algorithm/Array.java
+++ src/main/java/liye/carlos/myToolProcess/algorithm/Array.java
@@ -1,7 +1,6 @@
 package liye.carlos.myToolProcess.algorithm;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by liye3 on 2017/7/7.
@@ -78,7 +77,7 @@ public class Array {
      * https://leetcode.com/problems/k-diff-pairs-in-an-array/#/description
      */
     public int findPairs(int[] nums, int k) {
-        if (nums == null || nums.length == 0 || k < 0)   return 0;
+        if (nums == null || nums.length == 0 || k < 0) return 0;
 
         Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
@@ -102,4 +101,170 @@ public class Array {
         return count;
     }
 
+    /**
+     * 18. 4Sum
+     * <p>
+     * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+     * <p>
+     * Note: The solution set must not contain duplicate quadruplets.
+     * <p>
+     * For example, given array S = [1, 0, -1, 0, -2, 2], and target = 0.
+     * <p>
+     * A solution set is:
+     * [
+     * [-1,  0, 0, 1],
+     * [-2, -1, 1, 2],
+     * [-2,  0, 0, 2]
+     * ]
+     * <p>
+     * https://leetcode.com/problems/4sum/#/description
+     */
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        int len = nums.length;
+        if (nums == null || len < 4)
+            return res;
+
+        Arrays.sort(nums);
+        int max = nums[len - 1];
+        if (4 * nums[0] > target || 4 * max < target)
+            return res;
+
+        int i, z;
+        for (i = 0; i < len; i++) {
+            z = nums[i];
+            if (i > 0 && z == nums[i - 1])// avoid duplicate
+                continue;
+            if (z + 3 * max < target) // z is too small
+                continue;
+            if (4 * z > target) // z is too large
+                break;
+            if (4 * z == target) { // z is the boundary
+                if (i + 3 < len && nums[i + 3] == z)
+                    res.add(Arrays.asList(z, z, z, z));
+                break;
+            }
+
+            threeSumForFourSum(nums, target - z, i + 1, len - 1, res, z);
+        }
+        return res;
+
+    }
+
+    /**
+     * Find all possible distinguished three numbers adding up to the target
+     * in sorted array nums[] between indices low and high. If there are,
+     * add all of them into the ArrayList fourSumList, using
+     * fourSumList.add(Arrays.asList(z1, the three numbers))
+     */
+    private static void threeSumForFourSum(int[] nums, int target, int low, int high, ArrayList<List<Integer>> fourSumList,
+                                           int z1) {
+        if (low + 1 >= high)
+            return;
+
+        int max = nums[high];
+        if (3 * nums[low] > target || 3 * max < target)
+            return;
+
+        int i, z;
+        for (i = low; i < high - 1; i++) {
+            z = nums[i];
+            if (i > low && z == nums[i - 1]) // avoid duplicate
+                continue;
+            if (z + 2 * max < target) // z is too small
+                continue;
+
+            if (3 * z > target) // z is too large
+                break;
+
+            if (3 * z == target) { // z is the boundary
+                if (i + 1 < high && nums[i + 2] == z)
+                    fourSumList.add(Arrays.asList(z1, z, z, z));
+                break;
+            }
+
+            twoSumForFourSum(nums, target - z, i + 1, high, fourSumList, z1, z);
+        }
+
+    }
+
+    /**
+     * Find all possible distinguished two numbers adding up to the target
+     * in sorted array nums[] between indices low and high. If there are,
+     * add all of them into the ArrayList fourSumList, using
+     * fourSumList.add(Arrays.asList(z1, z2, the two numbers))
+     */
+    private static void twoSumForFourSum(int[] nums, int target, int low, int high, ArrayList<List<Integer>> fourSumList,
+                                         int z1, int z2) {
+
+        if (low >= high)
+            return;
+
+        if (2 * nums[low] > target || 2 * nums[high] < target)
+            return;
+
+        int i = low, j = high, sum, x;
+        while (i < j) {
+            sum = nums[i] + nums[j];
+            if (sum == target) {
+                fourSumList.add(Arrays.asList(z1, z2, nums[i], nums[j]));
+
+                x = nums[i];
+                while (++i < j && x == nums[i]) // avoid duplicate
+                    ;
+                x = nums[j];
+                while (i < --j && x == nums[j]) // avoid duplicate
+                    ;
+            }
+            if (sum < target)
+                i++;
+            if (sum > target)
+                j--;
+        }
+    }
+
+    /**
+     * 34. Search for a Range
+     * <p>
+     * Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
+     * Your algorithm’s runtime complexity must be in the order of O(log n).
+     * If the target is not found in the array, return [-1, -1].
+     * For example,
+     * Given [5, 7, 7, 8, 8, 10] and target value 8,
+     * return [3, 4].
+     * <p>
+     * https://leetcode.com/problems/search-for-a-range/#/description
+     */
+    public int[] searchRange1(int[] nums, int target) {
+        int index1 = binarySearch1(nums, target, 0, nums.length - 1);
+        int index2 = binarySearch2(nums, target, 0, nums.length - 1);
+        return new int[]{index1, index2};
+    }
+
+    private int binarySearch1(int[] nums, int target, int low, int high) {
+        if (low > high) return -1;
+        if (low == high) return nums[low] == target ? low : -1;
+        int mid = (high + low) / 2;
+        if (nums[mid] >= target) {
+            return binarySearch1(nums, target, low, mid);
+        } else {
+            return binarySearch1(nums, target, mid + 1, high);
+        }
+    }
+
+    private int binarySearch2(int[] nums, int target, int low, int high) {
+        if (low > high) return -1;
+        if (low == high) return nums[low] == target ? low : -1;
+        int mid = (high + low) / 2 + 1;
+        if (nums[mid] <= target) {
+            return binarySearch2(nums, target, mid, high);
+        } else {
+            return binarySearch2(nums, target, low, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 0, 0, 0, 0, 1};
+        System.out.println(fourSum(nums, 0));
+    }
 }
