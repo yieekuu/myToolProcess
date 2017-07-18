4e49bed0ed666dddf6605aa2bc393b7c601b1cbe
yieekuu <liye.carlos@gmail.com>
2017-07-18 09:53:31 +0800

    add

---
+++ src/main/java/liye/carlos/myToolProcess/dto/Element.java
@@ -0,0 +1,16 @@
+package liye.carlos.myToolProcess.dto;
+
+/**
+ * Created by liye3 on 2017/7/18.
+ */
+public class Element {
+    int val;
+    int idx;
+    int row;
+
+    public Element(int r, int i, int v) {
+        val = v;
+        idx = i;
+        row = r;
+    }
+}
