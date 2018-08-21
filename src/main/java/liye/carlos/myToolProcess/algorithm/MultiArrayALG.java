21e9830d091a286d5b9f8c35964017f3ab4ac920
liye18 <liye18@meituan.com>
2018-08-21 17:38:39 +0800

    add

--- src/main/java/liye/carlos/myToolProcess/algorithm/MultiArrayALG.java
+++ src/main/java/liye/carlos/myToolProcess/algorithm/MultiArrayALG.java
@@ -17,9 +17,9 @@ public class MultiArrayALG {
      */
     public static int scheduleCourse(int[][] courses) {
         Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-        PriorityQueue < Integer > queue = new PriorityQueue < > ((a, b) -> b - a);
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> b - a);
         int time = 0;
-        for (int[] c: courses) {
+        for (int[] c : courses) {
             if (time + c[0] <= c[1]) {
                 queue.offer(c[0]);
                 time += c[0];
@@ -31,10 +31,68 @@ public class MultiArrayALG {
         return queue.size();
     }
 
-public static void main(String[] args){
-        int[][] input = {{1,2},{2,3}};
-        int result = scheduleCourse(input);
- }
+    /**
+     * 78. Subsets
+     * Given a set of distinct integers, nums, return all possible subsets (the power set).
+     * Note: The solution set must not contain duplicate subsets.
+     * Example:
+     * Input: nums = [1,2,3]
+     * Output:
+     * [
+     * [3],
+     * [1],
+     * [2],
+     * [1,2,3],
+     * [1,3],
+     * [2,3],
+     * [1,2],
+     * []
+     * ]
+     * 提示：提交代码后，需要用简洁的语言解释一下代码思路~ 谢谢
+     * 历史题目和总结见公众号「每日一道算法题」
+     * https://leetcode.com/problems/subsets/description/
+     */
+    public static List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        for (int num : nums) {
+            int size = result.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> tmp = new ArrayList<>();
+                tmp.addAll(result.get(i));
+                tmp.add(num);
+                result.add(tmp);
+            }
+            List<Integer> addNum = new ArrayList<>();
+            addNum.add(num);
+            result.add(addNum);
+        }
+        result.add(new ArrayList<>());
+
+        return result;
+    }
+
+    public static List<List<Integer>> subsets2(int[] nums) {
+        if (nums == null) {
+            return new ArrayList<>();
+        }
+        int size = nums.length;
+        int resultSize = (int) Math.pow(2, size);
+        List<List<Integer>> result = new ArrayList<List<Integer>>(resultSize);
+        for (int i = 0; i < resultSize; i++) {
+            result.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < resultSize; j++) {
+                if ((j >> i & 1) == 0) {
+                    result.get(j).add(nums[i]);
+                }
+            }
+        }
+
+        return result;
+    }
+
+
 }
 
 
