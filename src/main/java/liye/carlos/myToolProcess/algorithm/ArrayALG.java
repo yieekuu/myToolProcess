f7cf23a750a8e4fd47592c0e1a982506b0cf149a
liye18 <liye18@meituan.com>
2018-08-28 15:52:45 +0800

    add

--- src/main/java/liye/carlos/myToolProcess/algorithm/ArrayALG.java
+++ src/main/java/liye/carlos/myToolProcess/algorithm/ArrayALG.java
@@ -672,11 +672,38 @@ public class ArrayALG {
         }
         int degree = Collections.max(countMap.values());
         Integer result = size;
-        for(Map.Entry<Integer,Integer> entry:countMap.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
             if (entry.getValue() == degree) {
-                int tmp = rightMap.get(entry.getKey()) - leftMap.get(entry.getKey())+1;
-                result = Math.min(tmp,result);
+                int tmp = rightMap.get(entry.getKey()) - leftMap.get(entry.getKey()) + 1;
+                result = Math.min(tmp, result);
             }
+        }
+
+        return result;
+    }
+
+    /**
+     * 442. Find All Duplicates in an Array
+     * Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+     * Find all the elements that appear twice in this array.
+     * Could you do it without extra space and in O(n) runtime?
+     * Example:
+     * Input:
+     * [4,3,2,7,8,2,3,1]
+     * Output:
+     * [2,3]
+     * 提示：提交代码后，需要用简洁的语言解释一下代码思路~ 谢谢
+     * 历史题目和总结见公众号「每日一道算法题」
+     * https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+     */
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                result.add(Math.abs(nums[i]));
+            }
+            nums[index] = -nums[index];
         }
 
         return result;
