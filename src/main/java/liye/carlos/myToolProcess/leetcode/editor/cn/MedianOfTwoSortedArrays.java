0a766208e770db3773088250a1d8e9ff1e111301
yieekuu <liye.carlos@gmail.com>
2024-06-27 17:28:10 +0800

    add new

--- src/main/java/liye/carlos/myToolProcess/leetcode/editor/cn/MedianOfTwoSortedArrays.java
+++ src/main/java/liye/carlos/myToolProcess/leetcode/editor/cn/MedianOfTwoSortedArrays.java
@@ -57,20 +57,6 @@ public class MedianOfTwoSortedArrays {
                     getKth(nums1, 0, m - 1, nums2, 0, n - 1, right)) * 0.5;
         }
 
-        private int getKth1(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
-            int len1 = end1 - start1 + 1;
-            int len2 = end2 - start2 + 1; //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
-            if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
-            if (len1 == 0) return nums2[start2 + k - 1];
-            if (k == 1) return Math.min(nums1[start1], nums2[start2]);
-            int i = start1 + Math.min(len1, k / 2);
-            int j = start2 + Math.min(len2, k / 2);
-            if (nums1[i] > nums2[j]) {
-                return getKth(nums1, start1, end1, nums2, j, end2, k - (j - start2));
-            } else {
-                return getKth(nums1, i, end1, nums2, start2, end2, k - (i - start1));
-            }
-        }
 
         /**
          * 获取两个数组中第k小数字
