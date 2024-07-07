47c128f1ab8963a16c8ad4a6b2e21690613c754f
yieekuu <liye.carlos@gmail.com>
2024-07-07 19:13:54 +0800

    二叉树层序遍历

---
+++ src/main/java/liye/carlos/myToolProcess/leetcode/editor/cn/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,95 @@
+//给定一个二叉树 root ，返回其最大深度。
+//
+// 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1,null,2]
+//输出：2
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点的数量在 [0, 10⁴] 区间内。 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 1842 👎 0
+
+
+package liye.carlos.myToolProcess.leetcode.editor.cn;
+
+import liye.carlos.myToolProcess.algorithm.Tree;
+import liye.carlos.myToolProcess.dto.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        Solution solution = new MaximumDepthOfBinaryTree().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            int depth = 0;
+            if (root == null) {
+                return depth;
+            }
+
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.add(root);
+            while (!queue.isEmpty()) {
+                int queueSize = queue.size();
+                //层遍历
+                for (int i = 0; i < queueSize; i++) {
+                    TreeNode tmp = queue.poll();
+                    if (tmp.left != null) {
+                        queue.add(tmp.left);
+                    }
+                    if (tmp.right != null) {
+                        queue.add(tmp.right);
+                    }
+                }
+                depth++;
+            }
+
+            return depth;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
